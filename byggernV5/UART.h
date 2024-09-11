d92da938cd0d8cb20d109660eccb0be2fba75fb0
August André Lukkassen <augusal@ntnu.no>
2024-09-11 17:52:06 +0200

    Initial commit in new repo

---
+++ byggernV5/UART.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <avr/io.h>
+#include <stdio.h>  // Required for FILE
+
+#define F_CPU 4915200UL
+#define FOSC 4915200UL
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
+
+void USART_Init( unsigned int ubrr );
+void USART_Transmit( unsigned char data );
+unsigned char USART_Receive( void );
+int uart_putchar(char c, FILE *stream);
+void init_printf(void);
---
+++ byggernV5/xmem.c
@@ -0,0 +1,22 @@
+#include "xmem.h"
+#include <avr/io.h>
+
+
+void xmem_init ( void ){
+	MCUCR |= (1 << SRE ); // enable XMEM
+	
+	SFIOR &= ~(1 << XMM0 | 1 << XMM1 ); // mask bits / reduce bus width
+	SFIOR |= (1 << XMM2 );
+}
+
+void xmem_write ( uint8_t data , uint16_t addr ){
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	ext_mem [ addr ]= data ;
+}
+
+
+uint8_t xmem_read ( uint16_t addr ){
+	volatile char * ext_mem = ( char *) BASE_ADDRESS ;
+	uint8_t ret_val = ext_mem [ addr ];
+	return ret_val ;
+}
